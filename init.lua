5522ae51d6cbfb1e858b988e0e5e86068fb0c0de
Ubuntu <ubuntu@ip-172-31-23-64.us-east-2.compute.internal>
2024-10-17 09:06:51 +0000

    remove slash in leader st

--- init.lua
+++ init.lua
@@ -333,7 +333,7 @@ require('lazy').setup({
       vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
       vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
       vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
-      vim.keymap.set('n', '<leader>st/', function()
+      vim.keymap.set('n', '<leader>st', function()
         -- Prompt the user to enter a file extension
         local file_extension = vim.fn.input 'File extension to search (e.g., *.js): '
 
