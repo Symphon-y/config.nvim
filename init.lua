a88d3e608119611e20856210c38b3d571035c129
Travis Redden <travis@Traviss-MacBook-Pro.local>
2024-10-24 01:44:11 -0500

    increased formatter timeout for large repos

--- init.lua
+++ init.lua
@@ -672,6 +672,9 @@ require('lazy').setup({
     },
     opts = {
       notify_on_error = false,
+      format = {
+        timeout_ms = 10000,
+      },
       format_on_save = function(bufnr)
         -- Disable "format_on_save lsp_fallback" for languages that don't
         -- have a well standardized coding style. You can add additional
